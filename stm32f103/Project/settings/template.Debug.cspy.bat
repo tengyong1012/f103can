77e7fcf96dbcc4f48437c8b699b88c58c410c29a
tengyong1012 <tengyong1012@163.com>
2017-12-05 20:48:46 +0800

    Signed-off-by: tengyong1012 <tengyong1012@163.com>

--- stm32f103/Project/settings/template.Debug.cspy.bat
+++ stm32f103/Project/settings/template.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\stm32f103\Project\settings\template.Debug.general.xcl" --backend -f "D:\stm32f103\Project\settings\template.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\stm32f103can\proj\f103can\stm32f103\Project\settings\template.Debug.general.xcl" --backend -f "D:\stm32f103can\proj\f103can\stm32f103\Project\settings\template.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\stm32f103\Project\settings\template.Debug.general.xcl" "--debug_file=%1" --backend -f "D:\stm32f103\Project\settings\template.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.2\common\bin\cspybat" -f "D:\stm32f103can\proj\f103can\stm32f103\Project\settings\template.Debug.general.xcl" "--debug_file=%1" --backend -f "D:\stm32f103can\proj\f103can\stm32f103\Project\settings\template.Debug.driver.xcl" 
 
 @echo off 
 :end
